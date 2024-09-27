3fa59d53d42deada67a990f4291bab38b0787853
Mamitiana2004 <99359394+Mamitiana2004@users.noreply.github.com>
2024-09-27 22:14:06 +0300

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,10 +24,8 @@
             imJs.smothScroll_Two();
             imJs.stickyAdjust();
             imJs.testimonialActivation();
-            imJs.contactForm();
             imJs.wowActive();
             imJs.awsActivation();
-            imJs.demoActive();
             imJs.activePopupDemo();
             imJs.onePageNav();
         },
